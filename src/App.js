76c1d8074fd64cfa3c653a3fe2da28c6723d01aa
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-09-28 06:43:19 +0900

    remove example stack

--- src/App.js
+++ src/App.js
@@ -43,11 +43,6 @@ function redo() {
         renderedStrokes.push(undoStrokes.pop());
 }
 
-var stack = [];
-stack.push(2);       // stack is now [2]
-stack.push(5);       // stack is now [2, 5]
-var i = stack.pop(); // stack is now [2]
-
 
 class App extends Component {
     componentDidMount() {
