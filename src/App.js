fc6ac8f5d63609742574eb935955e5fd8a677577
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-09-28 05:34:51 +0900

    react starter kit and basic js drawing board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
+
+//Canvas
+const default_margin = 10;
+const radius = 2;
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+var dragging = false;
+
+width = window.innerWidth - default_margin * 2;
+height = window.innerHeight - default_margin * 2;
+
+context.lineWidth = radius * 2;
+
+var putPoint = e => {
+    if (dragging) {
+        //draw line from previous to current point
+        context.lineTo(e.offsetX, e.offsetY);
+        context.stroke();
+
+        context.beginPath(); // move to new point
+        context.moveTo(e.offsetX, e.offsetY);
+    }
+};
+
+canvas.addEventListener('mousedown', () => dragging = true);
+canvas.addEventListener('mouseup', () => {
+    dragging = false;
+    context.beginPath();
+});
+canvas.addEventListener('mousemove', putPoint);
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.updateCanvas;
+    },
+
+    updateCanvas() {
+        const
+    },
+
+    render() {
+        return (
+            <canvas id="canvas" width={width} height={height}>Hello</canvas>
+        );
+    }
 }
 
 export default App;
