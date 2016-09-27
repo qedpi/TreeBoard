73d6ed28b55999bd5333db4baf96768ac9301b50
Robin Pham <robin@focus21.io>
2016-09-27 17:12:40 -0400

    Cleaned up stuff, refactored code so it works

--- src/App.js
+++ src/App.js
@@ -4,47 +4,47 @@ import './App.css';
 
 
 //Canvas
-const default_margin = 10;
-const radius = 2;
-
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-var dragging = false;
-
-width = window.innerWidth - default_margin * 2;
-height = window.innerHeight - default_margin * 2;
-
-context.lineWidth = radius * 2;
-
-var putPoint = e => {
-    if (dragging) {
-        //draw line from previous to current point
-        context.lineTo(e.offsetX, e.offsetY);
-        context.stroke();
-
-        context.beginPath(); // move to new point
-        context.moveTo(e.offsetX, e.offsetY);
-    }
-};
-
-canvas.addEventListener('mousedown', () => dragging = true);
-canvas.addEventListener('mouseup', () => {
-    dragging = false;
-    context.beginPath();
-});
-canvas.addEventListener('mousemove', putPoint);
 
 class App extends Component {
     componentDidMount() {
-        this.updateCanvas;
-    },
+        this.updateCanvas();
+    }
 
     updateCanvas() {
-        const
-    },
+      console.log('do something to update Canvas');
+      const radius = 2;
+
+      const canvas = document.getElementById('canvas');
+      const context = canvas.getContext('2d');
+
+      var dragging = false;
+
+      context.lineWidth = radius * 2;
+
+      var putPoint = e => {
+        if (dragging) {
+          //draw line from previous to current point
+          context.lineTo(e.offsetX, e.offsetY);
+          context.stroke();
+
+          context.beginPath(); // move to new point
+          context.moveTo(e.offsetX, e.offsetY);
+        }
+      };
+
+      canvas.addEventListener('mousedown', () => dragging = true);
+      canvas.addEventListener('mouseup', () => {
+        dragging = false;
+        context.beginPath();
+      });
+      canvas.addEventListener('mousemove', putPoint);
+
+    }
 
     render() {
+      const default_margin = 10;
+      let width = window.innerWidth - default_margin * 2;
+      let height = window.innerHeight - default_margin * 2;
         return (
             <canvas id="canvas" width={width} height={height}>Hello</canvas>
         );
