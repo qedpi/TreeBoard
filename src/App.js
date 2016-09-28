7a45ccedf6cb72da3cb0c833b816271af1b580e1
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-09-28 10:37:17 +0900

    minor tweaks

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Circle from './Drawable.js';       // todo: {Drawable, Point, Circle} doesn't work
+
 
 
 //Canvas
 
 const default_radius = 10;
+const default_color = 'black';
 
-
-class Drawable {
-    constructor() {
-        //metadata
-        this.timeCreated = new Date().getTime();    // utc time in seconds, exclude .getTime for proper time object
-        this.user = 1;                              // todo: set by session id?
-        this.session = 1;                           // todo: set by session id?
-        this.branch = 0;                            // primary, todo: set by interaction
-    }
-
-    toString() {
-        return `session: ${this.session}, user: ${this.user}, branch: ${this.branch}, at ${this.timeCreated}`;
-    }
-}
-
-class Point extends Drawable {
-    constructor(x, y) {
-        super.constructor();
-
-        //spatial
-        this.x = x;
-        this.y = y;
-
-        //aesthetics
-        this.r = default_radius;
-        this.color = 'black';
-    }
-    toString() {
-        //let detatils = '(' + this.x + ', ' + this.y + ')' + ' ' + this.color;
-        let details = `(${this.x}, ${this.y}) ${this.color}`;
-        return super.toString() + details;
-    }
-}
-
-/*
-class ColorPoint extends Point {
-    constructor(x, y, color) {
-        super(x, y);
-        this.color = color;
-    }
-    toString() {
-        return super.toString() + ' in ' + this.color;
-    }
-}
-*/
-
-var renderedStrokes = [];
-var undoStrokes = [];
+let renderedStrokes = [];
+let undoStrokes = [];
 
 function undo() {
     if (renderedStrokes.length > 0)
@@ -73,40 +30,46 @@ class App extends Component {
     }
 
     updateCanvas() {
-      console.log('do something to update Canvas');
-      const radius = 2;
+        console.log('do something to update Canvas');
+        const radius = 2;
 
-      const canvas = document.getElementById('canvas');
-      const context = canvas.getContext('2d');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
 
-      var dragging = false;
+        let dragging = false;
 
-      context.lineWidth = radius * 2;
+        context.lineWidth = radius * 2;
 
-      var putPoint = e => {
-        if (dragging) {
-          //draw line from previous to current point
-          context.lineTo(e.offsetX, e.offsetY);
-          context.stroke();
+        const drawPath = e => {
+            if (dragging) {
+                //draw line from previous to current point
+                context.lineTo(e.offsetX, e.offsetY);
+                context.stroke();
 
-          context.beginPath(); // move to new point
-          context.moveTo(e.offsetX, e.offsetY);
-        }
-      };
+                context.beginPath(); // move to new point
+                context.moveTo(e.offsetX, e.offsetY);
+            }
+        };
 
-      canvas.addEventListener('mousedown', () => dragging = true);
-      canvas.addEventListener('mouseup', () => {
-        dragging = false;
-        context.beginPath();
-      });
-      canvas.addEventListener('mousemove', putPoint);
+
+        const drawCircle = e => {
+            context.arc(e.offsetX, e.offsetY, radius * 2, 0, Math.PI * 2);
+            context.fill();
+        };
+
+        canvas.addEventListener('mousedown', () => dragging = true);
+        canvas.addEventListener('mouseup', () => {
+            dragging = false;
+            context.beginPath();
+        });
+        canvas.addEventListener('mousemove', drawPath);
 
     }
 
     render() {
-      const default_margin = 10;
-      let width = window.innerWidth - default_margin * 2;
-      let height = window.innerHeight - default_margin * 2;
+        const default_margin = 10;
+        let width = window.innerWidth - default_margin * 2;
+        let height = window.innerHeight - default_margin * 2;
         return (
             <canvas id="canvas" width={width} height={height}>Hello</canvas>
         );
